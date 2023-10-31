22b8a5a039696b7270579640ac630efb2c3169bb
bansheerubber <gary.cactus.email@gmail.com>
2023-10-31 16:00:53 -0700

    session manager

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,6 +7,8 @@ vim.keymap.set({ 'n', 'i', 'v' }, '<C-p>', builtin.find_files)
 vim.keymap.set('n', '<leader>f',
 	'<Cmd>Telescope file_browser sorting_strategy=ascending path=%:p:h select_buffer=true<CR>')
 
+vim.keymap.set('n', '<leader>a', '<Cmd>Telescope session-lens search_session<CR>')
+
 vim.keymap.set({ 'n', 'v', }, '<leader>t', function() builtin.treesitter({ ignore_symbols = { 'var', 'namespace' } }) end)
 
 -- find string in all files
