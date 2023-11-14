e0630893646b482afb0e78d2358fd6f05f1f0834
bansheerubber <gary.cactus.email@gmail.com>
2023-11-14 10:21:28 -0700

    attempt to fix cmp not loading properly
    edit: keybind registration /still/ doesn't work. i really hate how cmp does its keybinds.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,5 @@
 local lsp = require('lsp-zero')
+local cmp = require('cmp')
 
 lsp.set_sign_icons({
 	error = "√",
@@ -29,6 +30,121 @@ vim.keymap.set(
 	end,
 	{ desc = "Toggle lsp_lines" }
 )
+
+function setcmp()
+	cmp.setup.cmdline({
+		mapping = {
+			['<C-e>'] = {
+				i = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true
+				}),
+				c = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true
+				}),
+			},
+			['<CR>'] = {
+				i = cmp.config.disable,
+				c = cmp.config.disable,
+			},
+			['<Tab>'] = {
+				i = cmp.mapping.disable,
+				c = cmp.mapping.disable,
+			},
+		},
+	})
+
+	cmp.setup({
+		mapping = {
+			['<C-e>'] = {
+				i = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true
+				}),
+				c = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true
+				}),
+			},
+			['<C-Space>'] = {
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.abort(),
+			},
+			['<CR>'] = {
+				i = cmp.config.disable,
+				c = cmp.config.disable,
+			},
+			['<Tab>'] = {
+				i = cmp.mapping.disable,
+				c = cmp.mapping.disable,
+			},
+		},
+		preselect = cmp.PreselectMode.None,
+		formatting = {
+			fields = { 'menu', 'abbr', 'kind' },
+			format = function(entry, item)
+				local menu_icon = {
+					copilot                 = '₹',
+					nvim_lsp_signature_help = '∢',
+					nvim_lsp                = '∿',
+					nvim_lua                = '⅂',
+					buffer                  = '∀',
+					path                    = '∃',
+				}
+
+				local kinds = {
+					Text          = ' text',
+					Method        = 'methd',
+					Function      = ' func',
+					Constructor   = 'cnruc',
+					Field         = 'field',
+					Variable      = '  var',
+					Class         = 'class',
+					Interface     = 'intrf',
+					Module        = '  mod',
+					Property      = ' prop',
+					Unit          = ' unit',
+					Value         = '  val',
+					Enum          = ' enum',
+					Keyword       = 'kywrd',
+					Snippet       = ' snip',
+					Color         = 'color',
+					File          = ' file',
+					Reference     = '  ref',
+					Folder        = 'foldr',
+					EnumMember    = 'varnt',
+					Constant      = 'const',
+					Struct        = 'strct',
+					Event         = 'evnet',
+					Operator      = 'oprtr',
+					TypeParameter = ' type',
+				}
+
+				item.kind = kinds[item.kind]
+				item.menu = menu_icon[entry.source.name]
+				return item
+			end,
+		},
+		sources = {
+			{ name = 'nvim_lsp' },
+			{ name = 'nvim_lsp_signature_help' },
+			{ name = 'nvim_lua' },
+			-- { name = 'copilot', },
+			{ name = 'buffer' },
+			{ name = 'path' },
+		},
+		window = {
+			completion = {
+				winhighlight = "Normal:CmpNormal,FloatBorder:CmpFloatBorder,CursorLine:CmpCursorLine,Search:CmpSearch",
+			},
+			documentation = {
+				border = false,
+				winhighlight = "Normal:CmpDocNormal,FloatBorder:CmpDocFloatBorder",
+			},
+		},
+	})
+end
 
 require('luasnip.loaders.from_vscode').lazy_load()
 
@@ -70,119 +186,8 @@ lsp.on_attach(function(_, bufnr)
 	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
 	vim.keymap.set("n", "<leader>j", function() vim.lsp.buf.format() end, opts)
+
+	setcmp()
 end)
 
-local cmp = require('cmp')
-
-cmp.setup.cmdline({
-	mapping = {
-		['<C-e>'] = {
-			i = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true
-			}),
-			c = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true
-			}),
-		},
-		['<CR>'] = {
-			i = cmp.config.disable,
-			c = cmp.config.disable,
-		},
-		['<Tab>'] = {
-			i = cmp.mapping.disable,
-			c = cmp.mapping.disable,
-		},
-	},
-})
-
-cmp.setup({
-	mapping = {
-		['<C-e>'] = {
-			i = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true
-			}),
-			c = cmp.mapping.confirm({
-				behavior = cmp.ConfirmBehavior.Replace,
-				select = true
-			}),
-		},
-		['<C-Space>'] = {
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.abort(),
-		},
-		['<CR>'] = {
-			i = cmp.config.disable,
-			c = cmp.config.disable,
-		},
-		['<Tab>'] = {
-			i = cmp.mapping.disable,
-			c = cmp.mapping.disable,
-		},
-	},
-	preselect = cmp.PreselectMode.None,
-	formatting = {
-		fields = { 'menu', 'abbr', 'kind' },
-		format = function(entry, item)
-			local menu_icon = {
-				copilot                 = '₹',
-				nvim_lsp_signature_help = '∢',
-				nvim_lsp                = '∿',
-				nvim_lua                = '⅂',
-				buffer                  = '∀',
-				path                    = '∃',
-			}
-
-			local kinds = {
-				Text          = ' text',
-				Method        = 'methd',
-				Function      = ' func',
-				Constructor   = 'cnruc',
-				Field         = 'field',
-				Variable      = '  var',
-				Class         = 'class',
-				Interface     = 'intrf',
-				Module        = '  mod',
-				Property      = ' prop',
-				Unit          = ' unit',
-				Value         = '  val',
-				Enum          = ' enum',
-				Keyword       = 'kywrd',
-				Snippet       = ' snip',
-				Color         = 'color',
-				File          = ' file',
-				Reference     = '  ref',
-				Folder        = 'foldr',
-				EnumMember    = 'varnt',
-				Constant      = 'const',
-				Struct        = 'strct',
-				Event         = 'evnet',
-				Operator      = 'oprtr',
-				TypeParameter = ' type',
-			}
-
-			item.kind = kinds[item.kind]
-			item.menu = menu_icon[entry.source.name]
-			return item
-		end,
-	},
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'nvim_lsp_signature_help' },
-		{ name = 'nvim_lua' },
-		{ name = 'copilot', },
-		{ name = 'buffer' },
-		{ name = 'path' },
-	},
-	window = {
-		completion = {
-			winhighlight = "Normal:CmpNormal,FloatBorder:CmpFloatBorder,CursorLine:CmpCursorLine,Search:CmpSearch",
-		},
-		documentation = {
-			border = false,
-			winhighlight = "Normal:CmpDocNormal,FloatBorder:CmpDocFloatBorder",
-		},
-	},
-})
+setcmp()
